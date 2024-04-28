599b9c3bc650a9be1346c5dc6606448235f3f0f3
FelipeDorneles09 <157229416+FelipeDorneles09@users.noreply.github.com>
2024-04-28 14:48:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const key = "e3b225b73980fb70802a00b05b5e4f0f"
 
-function dadosNaTela (dados){
+function dadosNaTela(dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
     document.querySelector(".previsao").innerHTML = dados.weather[0].description
@@ -8,21 +8,29 @@ function dadosNaTela (dados){
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
-async function buscarCidade(cidade){
-
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
-
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
     dadosNaTela(dados)
-}   
+}
 
-function cliqueNoBotao(){
+function cliqueNoBotao() {
     const cidade = document.querySelector(".input-cidade").value
 
-    if (cidade !== ""){
-    buscarCidade(cidade)
+    if (cidade !== "") {
+        buscarCidade(cidade)
     }
 }
 
 window.onload = function() {
     buscarCidade("São Paulo");
+
+    const inputCidade = document.querySelector(".input-cidade");
+
+    
+    inputCidade.addEventListener("keypress", function(event) {
+       
+        if (event.key === "Enter") {
+            cliqueNoBotao(); 
+        }
+    });
 };
